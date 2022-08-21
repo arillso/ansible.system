17b9a743d35f047183b34f98c51a4d05d284d5f5
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2022-08-21 23:52:49 +0200

    fix lint

--- plugins/lookup/github_latest_release.py
+++ plugins/lookup/github_latest_release.py
@@ -6,9 +6,9 @@ from __future__ import absolute_import, division, print_function
 
 from json import JSONDecodeError, loads
 from re import compile as regex_compile
+
 from ansible.errors import AnsibleLookupError, AnsibleParserError
 from ansible.module_utils._text import to_native, to_text
-
 from ansible.module_utils.urls import open_url
 from ansible.plugins.lookup import LookupBase
 from ansible.utils.display import Display

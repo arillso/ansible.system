83c599921e29d6dde94db2cf292ec79f49397052
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2024-03-08 23:50:41 +0100

    fix: lint

--- plugins/filter/toml.py
+++ plugins/filter/toml.py
@@ -14,6 +14,7 @@ import json
 import sys
 from datetime import datetime
 
+# pylint: disable=import-error
 from ansible.errors import AnsibleFilterError
 from ansible.module_utils._text import to_text
 from ansible.module_utils.common._collections_compat import Mapping
